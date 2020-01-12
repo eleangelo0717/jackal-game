f85c82167f3158bb4142896624829e68693aa78e
Alexander Shelemetyev <ex.lxndr@gmail.com>
2020-01-12 15:32:58 +0700

    add cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, escape, request, jsonify
+from flask_cors import CORS
 
 from gameserver import GameServer
 from common.utils import GameEncoder
 
 app = Flask(__name__)
+CORS(app)
 app.json_encoder = GameEncoder
 
 GS = {}
